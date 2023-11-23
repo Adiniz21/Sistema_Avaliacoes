ac59ac0f64102aff18be3d4d1149b4d6a013bf55
Adiniz21 <arturdlmeira@gmail.com>
2023-11-23 17:25:13 -0300

    arrumando site

--- submit.php
+++ submit.php
@@ -3,13 +3,16 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 
-// Crie uma conexão
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Crie uma conexÃ£o
 $conn = new mysqli($servername, $username, $password);
 
-// Verifique a conexão
-/* if ($conn->connect_error) {
-    die("Conexão falhou: " . $conn->connect_error);
-} */
+// Verifique a conexÃ£o
+ if ($conn->connect_error) {
+    die("ConexÃ£o falhou: " . $conn->connect_error);
+} 
 
 // Crie o banco de dados
 $sql = "CREATE DATABASE IF NOT EXISTS minas_brasil";
@@ -22,8 +25,8 @@ $sql = "CREATE DATABASE IF NOT EXISTS minas_brasil";
 // Selecionar o banco de dados
 $conn->select_db("minas_brasil");
 
-// Crie a tabela para armazenar as avaliações, se não existir
-$sql = "CREATE TABLE IF NOT EXISTS avaliacoes (
+// Crie a tabela para armazenar as avaliaÃ§Ãµes, se nÃ£o existir
+$sql = "CREATE TABLE IF NOT EXISTS filial_01 (
     id INT AUTO_INCREMENT PRIMARY KEY,
     valor INT,
     data_hora DATETIME DEFAULT CURRENT_TIMESTAMP
@@ -35,35 +38,35 @@ $sql = "CREATE TABLE IF NOT EXISTS avaliacoes (
     echo "Erro ao criar tabela: " . $conn->error;
 }
  */
-// Obter valores do formulário
+// Obter valores do formulÃ¡rio
 $valor = $_POST['valor'];
 
 // Inserir dados no banco de dados, incluindo a hora atual
-$sql = "INSERT INTO avaliacoes (valor) VALUES ('$valor')";
+$sql = "INSERT INTO filial_01 (valor) VALUES ('$valor')";
 
 if ($conn->query($sql) === TRUE) {
-    // Exibir um alert com uma mensagem de sucesso e configurar um timeout para fechá-lo após 5 segundos
+    // Exibir um alert com uma mensagem de sucesso e configurar um timeout para fechÃ¡-lo apÃ³s 5 segundos
     echo "<script>
             var alerta = alert('Avaliação enviada com sucesso.');
             setTimeout(function(){ 
-                alerta.close(); // Fechar o alert após 5 segundos
+                alerta.close(); // Fechar o alert apÃ³s 5 segundos
                 window.location.href = 'index.html'; 
             }, 5000);
           </script>";
 } else {
     // Exibir um alert com uma mensagem de erro
     echo "<script>
-            var alertaErro = alert('Erro ao enviar avaliação: " . $conn->error . "');
+            var alertaErro = alert('Erro ao enviar avaliaÃ§Ã£o: " . $conn->error . "');
             setTimeout(function(){ 
-                alertaErro.close(); // Fechar o alert após 5 segundos
+                alertaErro.close(); // Fechar o alert apÃ³s 5 segundos
                 window.location.href = 'index.html'; 
             }, 5000);
           </script>";
 }
 
-// Fechar a conexão com o banco de dados
+// Fechar a conexÃ£o com o banco de dados
 $conn->close();
 
-// Redirecionar para a página index.html após o alert
+// Redirecionar para a pÃ¡gina index.html apÃ³s o alert
 echo "<script>window.location.href = 'index.html';</script>";
 ?>
